b1616f803a75d2b8b3c8fafcbd3c729bf2002a9a
Dunghedspi <dung.nv.soict@gmail.com>
2021-04-06 08:37:55 +0000

    has-text-grey-light

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,12 +4,21 @@
 　・チェックボックスにチェックが入っているか管理する
 　・チェックボックスにチェックが入っているかアイテムをグレーアウトする
 */
-function TodoItem(  {item} ) {
+const handleClickChecBox = (key) => {
+    const element = document.getElementById(key);
+    if(element.style.color === "grey"){
+      element.style.color = 'black';
+    }else{
+      element.style.color = "grey";
+    }
+};
+
+function TodoItem({item}) {
   return (
-    <label className="panel-block">
-            <input type="checkbox" />
+    <label className="panel-block" id={item.key}>
+            <input type="checkbox" onClick={() => handleClickChecBox(item.key)}/>
             {item.text}
-        </label>
+    </label>
   );
 }
 
